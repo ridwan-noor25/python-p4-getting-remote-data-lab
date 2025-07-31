1bae1eb449c041c9c60cc3cc1e96b97ba6e520a5
ridwan-noor25 <ridh2562@gmail.com>
2025-07-31 14:45:15 +0300

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,13 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Return as bytes to match the test
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()  # Returns list/dict
